f776efb2e05b5779566f263f01a3692bad879c95
Aswin <aswinckr@gmail.com>
2026-06-03 21:42:09 +0200

    Update Blab card to match Pick layout

---
+++ tests/check-blab-card.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+
+test -f images/blab-cover.png
+
+rg -q 'class="w-layout-layout quick-stack-cards wf-layout-layout"' index.html
+rg -q '<p[^>]*class="heading-2">Chat app Blab</p>' index.html
+rg -q 'src="images/blab-cover.png"' index.html
+rg -q 'Designing and building with AI language-practice chat app\.' index.html
+rg -q 'Product Design' index.html
+rg -q 'Flutter MVP' index.html
+rg -q 'AI-assisted build' index.html
+
+rg -q '<p[^>]*class="heading-2">Chat app Blab</p>' blab-case-study.html
+rg -q 'src="images/blab-cover.png"' blab-case-study.html
+rg -q 'Designing and building with AI language-practice chat app\.' blab-case-study.html
+! rg -q 'blab-hero|blab-section|Blab: from learning idea|Case study in progress' blab-case-study.html
